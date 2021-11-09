f3ac033adc1db2913a608c2840881f0377d4299b
ahamm-prequal2021 <a.hamm@codeur.online>
2021-11-09 17:21:21 +0100

    contact me glassmorphism

--- actions/database.php
+++ actions/database.php
@@ -1,3 +1,7 @@
 <?php
 
-$db = new PDO('mysql:host=localhost;dbname=cv', 'root', '');
+if(preg_match ('#^/alexandreh.promo-90.codeur.online/#', $_SERVER['REQUEST_URI'])){
+    $db = new PDO('mysql:host=localhost;dbname=alexandreh_cv;charset=utf8;', 'alexandreh', 'qCPwobB/1HD+vw==');
+}else{
+    $db = new PDO('mysql:host=localhost;dbname=cv;charset=utf8;', 'root', '');
+}
