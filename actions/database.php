3d2d99d4160d8767bc9e058a27ac59bae5df42d3
ahamm-prequal2021 <a.hamm@codeur.online>
2021-11-10 09:56:14 +0100

    guestbook

--- actions/database.php
+++ actions/database.php
@@ -2,4 +2,3 @@
 
     $db = new PDO('mysql:host=localhost;dbname=alexandreh_cv;charset=utf8;', 'alexandreh', 'qCPwobB/1HD+vw==');
     // $db = new PDO('mysql:host=localhost;dbname=cv;charset=utf8;', 'root', '');
-}
