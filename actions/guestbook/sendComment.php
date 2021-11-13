7b70eab066eb9da9f8ed231f839226e1f6551630
ahamm-prequal2021 <a.hamm@codeur.online>
2021-11-13 16:02:49 +0100

    mail on new guestbook msg

--- actions/guestbook/sendComment.php
+++ actions/guestbook/sendComment.php
@@ -9,6 +9,9 @@ if(isset($_POST['sendCom'])){
         $send = $db->prepare('INSERT INTO guestbook ( name, comment ) VALUES ( ?, ? )');
         
         $send->execute(array($_POST['name'], $_POST['comment']));
+
+        $new = 'Il y a un nouveau message dans le livre d\'or sur le portfolio';
+        $retour = mail('a.hamm@codeur.online', 'Nouveau message sur le livre d\'or', $new, 'From : Alex H. Portfolio');
         
     }
 
